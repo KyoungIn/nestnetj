16ac8f9d1b9b5d09279782eeda9dd57503fddeda
KyoungIn <kykihk@nate.com>
2014-08-03 16:45:48 +0900

    8.3

---
+++ src/com/example/smart_door/NowHomeState.java
@@ -0,0 +1,17 @@
+package com.example.smart_door;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class NowHomeState extends Activity{
+		
+		Button nowHomeState;
+		Button remoteControl;
+		Button setting;
+		protected void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.now_state);
+			
+		}
+}
